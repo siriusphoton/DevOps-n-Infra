34d6a1cbbc240eaf7f6b88f8f5629442160b0a42
capstonevm <sreesai072@gmail.com>
2026-01-31 08:40:57 +0000

    Add log backups and config cloud watch

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -47,3 +47,11 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+resource "aws_iam_role_policy_attachment" "cw_agent" {
+
+  role = aws_iam_role.ec2_role.name
+
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+
+}
