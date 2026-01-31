7c795cf6bd44823f836151ce906d6b45e8900883
capstonevm <sreesai072@gmail.com>
2026-01-31 12:27:45 +0000

    dockerize the app enable ecr

---
+++ terraform/ecr.tf
@@ -0,0 +1,23 @@
+resource "aws_ecr_repository" "app_repo" {
+
+  name = "secure-doc-repo"
+
+  image_tag_mutability = "MUTABLE"
+
+
+
+  image_scanning_configuration {
+
+    scan_on_push = true
+
+  }
+
+}
+
+
+
+output "repository_url" {
+
+  value = aws_ecr_repository.app_repo.repository_url
+
+}
